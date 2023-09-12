4d736d43ce3e6e6ce3ef25b2acef518bd12caec1
Francielli Barbara Pinto <francielli.pinto@ifsuldeminas.edu.br>
2023-09-12 12:06:21 -0300

    Corrigindo o nome do controller

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/controller/GitIntroController.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/controller/GitIntroController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GitIntro {
+public class GitIntroController {
     @GetMapping("/intro")
     public String intro(){
         return "Introdução ao Git e GitHub";
