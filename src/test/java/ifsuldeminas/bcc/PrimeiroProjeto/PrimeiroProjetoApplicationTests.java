bce313f8b8d4a2d5d38e2a153825b139285a6f64
Francielli Barbara Pinto <francielli.pinto@ifsuldeminas.edu.br>
2023-09-12 11:33:41 -0300

    Primeiro commit

---
+++ src/test/java/ifsuldeminas/bcc/PrimeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.bcc.PrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
